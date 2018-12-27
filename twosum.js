b1b1f5a16962f5b94637ef961334787177d64707
Mikaylahasal <mikayla.k.hasal@gmail.com>
2018-12-27 10:07:24 -0800

    working solution

--- twosum.js
+++ twosum.js
@@ -12,11 +12,21 @@ return [0, 1].
 */
 
 var twoSum = function(nums, target) {
-  //create hash
-  //loop through array,
-    //set each index to key
-  //loop again
-    //set difference to target - arr[i]
-  //check hash to see if diff is a prop && if hash[diff] doesnt equal i
-    //return [i, hash[diff]]
+    //create hash
+    var hash = {};
+    //loop through array,
+    for (var i = 0; i < nums.length; i++) {
+        //set each index to key
+        let current = nums[i];
+        hash[current] = i;
+    }
+    //loop again
+    for (var i = 0; i < nums.length; i++) {
+        //set difference to target - arr[i]
+        let diff = target - nums[i];
+        //check hash to see if diff is a prop && if hash[diff] doesnt equal i
+        if (hash.hasOwnProperty(diff) && hash[diff] !== i) {
+            return [i, hash[diff]]
+      }
+    }
 };
